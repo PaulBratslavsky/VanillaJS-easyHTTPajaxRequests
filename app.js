b9bb591dad1a69d44e0dd5e28dba4ddfae061bf9
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-11-15 13:16:29 -0800

    final commit

--- app.js
+++ app.js
@@ -1,17 +1,79 @@
 console.log('app.js')
 
 const http = new easyHTTP;
-const url = 'https://jsonplaceholder.typicode.com/posts';
+const getPostsUrl = 'https://jsonplaceholder.typicode.com/posts';
+const getSinglePostUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+
+const addPostUrl = 'https://jsonplaceholder.typicode.com/posts';
+const addPutUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+
+const deletePostUrl = 'https://jsonplaceholder.typicode.com/posts/10';
+
+
+/**********************************************
+    GET ALL POSTS
+ *********************************************/
 /*
-// GET
-http.get(url, function(error, respone) {
+http.get(getPostsUrl, function(error, respone) {
     if (error) {
-        console.error(error, 'Response');
+        console.error(error, 'Error');
+    } else {
+        console.log(respone, 'Response');
+    }
+});
+
+*/
+
+/**********************************************
+    GET SINGLE POST
+ *********************************************/
+/*
+http.get(getSinglePostUrl, function(error, respone) {
+    if (error) {
+        console.error(error, 'Error');
     } else {
         console.log(respone, 'Response');
     }
 });
 */
+/**********************************************
+    ADD SINGLE POST
+ *********************************************/
 
-// POST
-http.post('test', (response) => { console.log(response)});
+// Create data to send
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custome post'
+}
+/*
+http.post(addPostUrl, data, (error, respone) => {
+    if (error) {
+        console.error(error, 'Response Error!!!');
+    } else {
+        console.log(respone, 'Response Success');
+    }
+});
+*/
+/**********************************************
+    PUT REQUEST: UPDATE SINGLE POST
+ *********************************************/
+/*
+http.put(addPutUrl, data, (error, respone) => {
+    if (error) {
+        console.error(error, 'Response Error!!!');
+    } else {
+        console.log(respone, 'Response Success');
+    }
+})
+*/
+
+/**********************************************
+    DELETE REQUEST: DELETE SINGLE POST
+ *********************************************/
+http.delete(deletePostUrl, function(error, respone) {
+    if (error) {
+        console.error(error, 'Error');
+    } else {
+        console.log(respone, 'Response');
+    }
+});
--- easyHTTP.js
+++ easyHTTP.js
@@ -4,7 +4,9 @@ function easyHTTP() {
     this.http = new XMLHttpRequest();
 }
 
-// GET REQUEST
+/**********************************************
+    GET REQUEST
+ *********************************************/
 easyHTTP.prototype.get = function(url, callback) {
     this.http.open('GET', url, true);
 
@@ -29,12 +31,92 @@ easyHTTP.prototype.get = function(url, callback) {
     this.http.send();
 }
 
-// POST REQUEST
-easyHTTP.prototype.post = function(url, callback) {
-    console.log(url)
-    callback('Hello')
+/**********************************************
+    POST REQUEST: ADD SINGLE POST
+ *********************************************/
+easyHTTP.prototype.post = function(url, data, callback) {
+    console.log(data);
+
+    // Initialize
+    this.http.open('POST', url, true);
+
+    // Set this to self variable to pass the context
+    const self = this.http;
+
+    // Set content type: we are working with JSON so thats what we have to set to
+    this.http.setRequestHeader('Content-type', 'application/json');
+    
+    this.http.onload = function() {
+        // Check Status For Success
+        if ( self.status === 201 ) {
+            callback(null, self.status);
+        } else {
+            console.log('It Failed.')
+            const error = `Error: ${self.status}`;
+            callback(error, null);
+        }
+        
+    };
+
+    // Pass object to send must parse to string first
+    this.http.send(JSON.stringify(data));
 }
 
-// PUT REQUEST 
+/**********************************************
+    PUT REQUEST
+ *********************************************/
+easyHTTP.prototype.put = function(url, data, callback) {
+    console.log(data);
 
-// DELETE REQUEST
+    // Initialize
+    this.http.open('PUT', url, true);
+
+    // Set this to self variable to pass the context
+    const self = this.http;
+
+    // Set content type: we are working with JSON so thats what we have to set to
+    this.http.setRequestHeader('Content-type', 'application/json');
+    
+    this.http.onload = function() {
+        // Check Status For Success
+        if ( self.status === 200 ) {
+            callback(null, self.status);
+            console.log(this.responseText, "DATA ADDED");
+        } else {
+            console.log('It Failed.')
+            const error = `Error: ${self.status}`;
+            callback(error, null);
+        }
+        
+    };
+
+    // Pass object to send must parse to string first
+    this.http.send(JSON.stringify(data));
+}
+
+/**********************************************
+    DELETE REQUEST
+ *********************************************/
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+
+    // Pass the context of this to new function.
+    console.log(this, 'get this');
+
+    const self = this;
+
+    this.http.onload = function() {
+        console.log(this, 'onload this');
+        console.log(self, 'self');
+        // Check Status
+        if ( self.http.status === 200 ) {
+            callback(null, self.http.responseText);
+        } else {
+            console.log('It Failed.')
+            const error = `Error: ${self.http.status}`;
+            callback(error, null);
+        }
+    };
+
+    this.http.send();
+}
